f31f0e957f63b4187ddf6800448952d753f41cea
pilar <pilar.rus.izquierdo@gmail.com>
2020-03-20 12:17:32 +0100

    First commit

---
+++ src/main/java/pruebajar/pojos/Person.java
@@ -0,0 +1,44 @@
+package pruebajar.pojos;
+
+/**
+ * POJO Person
+ */
+public class Person {
+
+    private String name;
+    private String surName;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, String surName, int age) {
+        this.name = name;
+        this.surName = surName;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
